bf2e2b2cb4686888091765106f4ec81989aaa284
Mikhail Korolev <self@mkrl.xyz>
2025-03-11 12:14:24 +0100

    Switch model

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -13,7 +13,7 @@ export async function POST(req: Request) {
 
 
   const result = streamText({
-    model: openai('gpt-4o'),
+    model: openai('gpt-4o-mini'),
     toolCallStreaming: true,
     tools: {
       createSandbox: createSandboxTool,
